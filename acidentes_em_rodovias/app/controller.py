b0d2ded6292de6c2db5c47d8b2722fb72291c0fb
Matheus Fonseca <1matheusfonseca@gmail.com>
2013-09-16 13:59:53 -0300

    Dojo/Kata do dia 16-09

--- acidentes_em_rodovias/app/controller.py
+++ acidentes_em_rodovias/app/controller.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*- 
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+class Pessoa():
+	def __init__(self, nome, idade, sexo):
+		self.nome = nome
+		self.idade = idade
+		self.sexo = sexo
+
+	def __str__(self):
+		return "Nome = {0}, Idade = {1}, Sexo = {2}".format(self.nome, self.idade, self.sexo)
