5b22f1877f745c850f14337846ed650048d53532
Luiz <ziuloliveira@gmail.com>
2013-09-21 16:39:22 -0300

    parser mais generico

--- acidentes_em_rodovias/extractor/parser/parser.py
+++ acidentes_em_rodovias/extractor/parser/parser.py
@@ -2,18 +2,36 @@
 #-*- encoding: utf-8 -*-
 
 import csv, sys
+import odict
 
-def parser(filename):
-	with open(filename, 'rb') as filename:
-	    reader = csv.reader(filename, delimiter = ';', quotechar = '"')
-	    try:
-	        for row in reader:
-	        	print row[0] + "\t" + row[1]
+class parser(object):
+	data={}
+	"""
+	@package Parser
+	construtor das informações adquiridas através do arquivo CSV.
+	
+	"""
+	def __init__(self,filename):
+		with open(filename, 'rb') as filename:
+			reader = csv.reader(filename, delimiter = ';', quotechar = '\"')
+			try:
+				self.data=odict.odict()
+				for i in reader.next():
+					self.data[str(i)] = []
+				for row in reader:
+					for (i,d) in zip(xrange(len(row)),self.data.keys()):
+						self.data[d].append(row[i])
+			except csv.Error as e:
+				sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+			
+			self.data=dict(self.data)
+			
 
-	    except csv.Error as e:
-	        sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+if __name__ == "__main__":
+	filename = "estadofisico.csv"
+	estado_fisico=parser(filename)
+	print estado_fisico.data
 
+	Pessoa = parser("tabelaPessoa.csv")
 
-
-filename = "estadofisico.csv"
-parser(filename)
+	print Pessoa.data.keys()
