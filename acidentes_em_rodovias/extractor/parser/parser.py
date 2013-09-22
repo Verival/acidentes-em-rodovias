07577a8766cd25dcade0a8959aed3b1a5ecca4d1
Luiz <ziuloliveira@gmail.com>
2013-09-22 16:45:01 -0300

    inclusão do suporte a pandas e alguns exemplos de uso nos comentarios

--- acidentes_em_rodovias/extractor/parser/parser.py
+++ acidentes_em_rodovias/extractor/parser/parser.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 #-*- encoding: utf-8 -*-
 
-import csv, sys
-import odict
-
+try:
+	import csv, sys
+	import odict
+	from pandas import *  
+	import matplotlib.pyplot as plt
+except ImportError , ex:
+	print ex
+	print "try:\n sudo pip install " +  str(ex).split(' ')[-1]
+	quit()
+	
 class parser(object):
 	data={}
 	"""
@@ -28,14 +35,46 @@ class parser(object):
 			
 
 if __name__ == "__main__":
+	'''Fazendo de uso da classe parser'''
 	filename = "estadofisico.csv"
 	estado_fisico=parser(filename)
 	print estado_fisico.data
 
+	'''Fazendo de uso da pandas'''
+	#Fazendo leitura de CSV
+	experimentDF = read_csv("pessoa_2_Semestre_2012.csv",delimiter = ';', na_values=["(null)"])  
+
+	# Plotando dados
+	experimentDF['pesidade'][:50].plot() #apenas os 50 primeiros dados
+	plt.show() #Forçar amostrar da imagem
+		
 	'''
-	Padrão para expansão de leitura de data
-	\warning:  Arquivo com pouco mais de 700mb deixado de fora do GIT por razões obvias
-	Pessoa = parser("tabelaPessoa.csv")
+	#imprimindo DataFrame
+	print experimentDF
 	
-	print Pessoa.data.keys()
+	#imprimindo chaves do DataFrame
+	print experimentDF.keys()
+	
+	#convertendo para dicionario
+	Pessoa = experimentDF.to_dict()
+	print "Quantidade de colunas: ",
+	print len(Pessoa.keys())
+
+	#Utilizando as chaves do dict para selecionar informação desejada
+	print experimentDF[Pessoa.keys()[0]]
+
+	#summary statistics de uma coluna em especifico
+	print experimentDF['peslesao'].describe()
+	
+	#imprime a média de todos os objetos
+	print experimentDF.mean()
+
+	experimentDF = read_csv(filename,delimiter = ';', na_values=["(null)"])  
+	#convertendo para HTML
+	Pessoa_html = experimentDF.to_html()
+	f = open('workfile.html', 'w')
+	f.write(Pessoa_html)
 	'''
+	
+	
+	
