feed19d2a6e1cf5bf1bc89690e00f80153c00cf9
Matheus Fonseca <1matheusfonseca@gmail.com>
2013-10-02 11:25:38 -0300

    O ETL foi atualizado para se adequar a nova disposição de dados no portal

--- acidentes_em_rodovias/ETL/extract-transform-load.sh
+++ acidentes_em_rodovias/ETL/extract-transform-load.sh
@@ -10,7 +10,7 @@
 #	sudo /etc/init.d/apparmor restart
 #	sudo service mysql restart
 # 2) Crie uma base de dados no MySQL com o nome 'acidentes_rodovias' e coloque o collation dele como utf8_bin
-# 3) Caso já tenha os arquivos BRBRASIL.zip, tabelaPessoa.zip, estadofisico.csv e causaacidente.csv baixados, comente as linhas que dão 'wget' neles e coloque os arquivos em $DATA_DIR
+# 3) Caso já tenha os arquivos *.zip baixados, comente a linha que da 'wget' neles e coloque os arquivos em $DATA_DIR/acidentes_rodovias/zips (crie as pastas caso elas não existam)
 # 4) Execute o programa:
 #	Uso: ./extract-transform-load.sh <BD_USER> <BD_PASS> <DATA_DIR>
 #	Ex: ./extract-transform-load.sh root 123456 /tmp
@@ -26,36 +26,24 @@ DATA_DIR=$3
 
 function prepare_enviroment {
 	echo -e "\nDownloading data..."
-	wget -P $DATA_DIR http://repositorio.dados.gov.br/transportes-transito/transito/BRBRASIL.zip
-	wget -P $DATA_DIR https://dl.dropboxusercontent.com/u/18364240/estadofisico.csv
-	wget -P $DATA_DIR http://repositorio.dados.gov.br/transportes-transito/transito/causaacidente.csv
-	wget -P $DATA_DIR http://repositorio.dados.gov.br/transportes-transito/transito/tabelaPessoa.zip
+	mkdir -p $WORK_DIR/zips
+	wget -P $WORK_DIR/zips/ -r --no-parent -l 1 --accept="*.zip" --no-directories  http://repositorio.dados.gov.br/transportes-transito/transito/
 
-	if [[ ! -f "$DATA_DIR/BRBRASIL.zip" || ! -f "$DATA_DIR/tabelaPessoa.zip" || ! -f "$DATA_DIR/estadofisico.csv" || ! -f "$DATA_DIR/causaacidente.csv" ]]
+	if [[ ! -f "$WORK_DIR/zips/dominios.zip" || ! -f "$WORK_DIR/zips/brbrasil_1_semestre_2008.zip" || ! -f "$WORK_DIR/zips/pessoas_e_veiculos.zip" ]]
 	then
-		echo "ERROR: Files $DATA_DIR/BRBRASIL.zip, $DATA_DIR/tabelaPessoa.zip, $DATA_DIR/causaacidente.csv or $DATA_DIR/estadofisico.csv dont exist"
+		echo "ERROR: Files $DATA_DIR/zips/dominio.zip, $DATA_DIR/zips/brbrasil*.zip or $DATA_DIR/zips/pessoas_e_veiculos.zip dont exist"
 		exit -1
 	fi
 
 	echo -e "\nUnziping data..."
-	unzip -o -d $DATA_DIR $DATA_DIR/BRBRASIL.zip
-	for i in $(ls $DATA_DIR/BRBRASIL/*.zip)
-	do 
-		unzip -o -d $DATA_DIR/BRBRASIL $i
-	done
-	unzip -o -d $DATA_DIR $DATA_DIR/tabelaPessoa.zip
-
-	cp -f $DATA_DIR/estadofisico.csv $DATA_DIR/BRBRASIL
-	cp -f $DATA_DIR/tabelaPessoa.csv $DATA_DIR/BRBRASIL/pessoa.csv
-	cp -f $DATA_DIR/causaacidente.csv $DATA_DIR/BRBRASIL
-	rm -f $DATA_DIR/BRBRASIL/*.zip $DATA_DIR/BRBRASIL/*.pdf
+	unzip -o -d $WORK_DIR $WORK_DIR/zips/\*.zip
+	mv $WORK_DIR/tabelaPessoa.csv $WORK_DIR/pessoa.csv
 
 	echo -e "\nStandardizing delimiters..."
-	sed -i 's/|/;/g' "$DATA_DIR/BRBRASIL/veiculo.csv"
-	sed -i 's/|/;/g' "$DATA_DIR/BRBRASIL/ocorrenciaveiculo.csv"
+	sed -i 's/|/;/g' "$WORK_DIR/ocorrenciaveiculo.csv"
 
 	echo -e "\nStandardizing null cells to \N..."
-	for i in $(ls $DATA_DIR/BRBRASIL/**/*.csv)
+	for i in $(ls $WORK_DIR/**/*.csv)
 	do 
 	 	sed -i 's/(null)/\\N/g' $i
 		sed -i 's/;;/;\\N;/g' $i
@@ -63,7 +51,7 @@ function prepare_enviroment {
 	 	sed -i 's/\"\s\+\"/\\N/g' $i
 	 	sed -i 's/;\\ ;/;\\N;/g' $i
 	done
-	for i in $(ls $DATA_DIR/BRBRASIL/*.csv)
+	for i in $(ls $WORK_DIR/*.csv)
 	do 
 		sed -i 's/(null)/\\N/g' $i
 	 	sed -i 's/;;/;\\N;/g' $i
@@ -88,39 +76,47 @@ function prepare_db {
 	fi
 }
 
-function load_non_semiannual_data {
-	for TABLE in corveiculo localbr estadofisico marcadeveiculo municipio ocorrenciaveiculo tipoAcidente tipoApreensao tipoAreaEspecial tipoComunicacao tipocrime tipodetencao tipodocumento tipoenvolvido tipolocalidade tipoobra tipopontomedico tipopontonotavel tiporeceptor tiposinalizacao tipounidadeoperacional tipoveiculo uf unidadeoperacional veiculo causaacidente pessoa
+function load_domain_data {
+	for TABLE in ocorrenciaveiculo veiculo pessoa
 	do
-		echo -e "\nLoading non-semiannual table \"$TABLE\" data..."
-		FILE="$DATA_DIR/BRBRASIL/$TABLE.csv"
+		echo -e "\nLoading domain table \"$TABLE\" data..."
+		FILE="$WORK_DIR/$TABLE.csv"
 		QUERY="LOAD DATA INFILE '$FILE' IGNORE INTO TABLE \`$TABLE\` CHARACTER SET utf8 FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES;"
 		mysql -u $DB_USER --password=$DB_PASS acidentes_rodovias -e "$QUERY"
 	done
 }
 
-function load_semiannual_data {
+function load_non_biannual_data {
+	for TABLE in corveiculo localbr estadofisico marcadeveiculo municipio tipoAcidente tipoApreensao tipoAreaEspecial tipoComunicacao tipocrime tipodetencao tipodocumento tipoenvolvido tipolocalidade tipoobra tipopontomedico tipopontonotavel tiporeceptor tiposinalizacao tipounidadeoperacional tipoveiculo uf unidadeoperacional causaacidente
+	do
+		echo -e "\nLoading non-biannual table \"$TABLE\" data..."
+		FILE="$WORK_DIR/$TABLE.csv"
+		QUERY="LOAD DATA INFILE '$FILE' IGNORE INTO TABLE \`$TABLE\` CHARACTER SET utf8 FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES;"
+		mysql -u $DB_USER --password=$DB_PASS acidentes_rodovias -e "$QUERY"
+	done
+}
+
+function load_biannual_data {
 	i=0
-	for HALF in $(ls -d /tmp/BRBRASIL/brbrasil* | grep -E -o "_(1|2)_" )
+	for HALF in $(ls -d $WORK_DIR/brbrasil* | grep -E -o "_(1|2)_" )
 	do
 		HALFS[$i]="${HALF:1:1}" 
 		((i++))
 	done
 	i=0
-	for YEAR in $(ls -d /tmp/BRBRASIL/brbrasil* | grep -E -o "[0-9]{4}")
+	for YEAR in $(ls -d $WORK_DIR/brbrasil* | grep -E -o "[0-9]{4}")
 	do
 		YEARS[$i]="$YEAR"
 		((i++))
 	done
 
-	for TABLE in ocorrencia ocorrenciaacidente ocorrenciaPessoa #pessoa 
+	for TABLE in ocorrencia ocorrenciaacidente ocorrenciaPessoa
 	do
-		echo -e "\nLoading semiannual table \"$TABLE\" data..."
-		# ID_COLUMN="$(head -1 $DATA_DIR/BRBRASIL/brbrasil_${HALFS[0]}_semestre_${YEARS[0]}/${TABLE}_${HALFS[0]}_Semestre_${YEARS[0]}.csv | sed -e 's/;/\n/g' | head -1)"
-		# OTHER_COLUMNS="$(head -1 $DATA_DIR/BRBRASIL/brbrasil_${HALFS[0]}_semestre_${YEARS[0]}/${TABLE}_${HALFS[0]}_Semestre_${YEARS[0]}.csv | sed -e 's/;/\n/g' | tail -n+2 | sed ':a;N;$!ba;s/\n/\`,\`/g')"
+		echo -e "\nLoading biannual table \"$TABLE\" data..."
 
 		for (( i=0; i < ${#YEARS[@]}; i++ ))
 		do
-			FILE="$DATA_DIR/BRBRASIL/brbrasil_${HALFS[$i]}_semestre_${YEARS[$i]}/${TABLE}_${HALFS[$i]}_Semestre_${YEARS[$i]}.csv"
+			FILE="$WORK_DIR/brbrasil_${HALFS[$i]}_semestre_${YEARS[$i]}/${TABLE}_${HALFS[$i]}_Semestre_${YEARS[$i]}.csv"
 			QUERY="LOAD DATA INFILE '$FILE' IGNORE INTO TABLE \`$TABLE\` CHARACTER SET utf8 FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES SET \`sem\` = ${HALFS[$i]}, \`ano\` = ${YEARS[$i]};"
 			echo "Loading ${HALFS[$i]}-${YEARS[$i]}"
 			mysql -u $DB_USER --password=$DB_PASS acidentes_rodovias -e "$QUERY"
@@ -135,11 +131,14 @@ function main {
 		exit -1
 	fi
 	[[ ! "$DATA_DIR" ]] && DATA_DIR=/tmp
+	
+	WORK_DIR="$DATA_DIR/acidentes_rodovias"
 
 	#prepare_enviroment
 	prepare_db
-	load_non_semiannual_data
-	load_semiannual_data
+	load_domain_data
+	load_non_biannual_data
+	load_biannual_data
 
 	echo -e "\nFinish!"
 }
