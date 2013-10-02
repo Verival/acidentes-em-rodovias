9c6b5bd48276cba56c5d5eae6b713ffe20a40a53
msfernandes <matheus.souza.fernandes@gmail.com>
2013-10-02 00:44:04 -0300

    Adicionado codigo com exemplo de utilização da biblioteca MySQLdb

--- acidentes_em_rodovias/app/dao.py
+++ acidentes_em_rodovias/app/dao.py
@@ -1,1 +1,26 @@
 # -*- coding: utf-8 -*- 
+
+#Codigo de Exemplo para uso do MySQLdb
+
+import MySQLdb
+
+connection = MySQLdb.connect('localhost', 'root', '1234')	# Aqui inicia a conexão com o Banco.
+															# Os parametros são: servidor, usuario e senha.
+
+connection.select_db('test')	# Seleciona o Banco de Dados.
+
+query = connection.cursor()	# Cursor que será usado para executar as queries.
+
+query.execute('SELECT * FROM test_table')	# Query que será executada, no caso, um SLECT simples.
+
+# Agora vem a parte magica :)
+
+result = query.fetchone()	# Pega só uma linha. Se voce continuar usando o 'fetchone()', ele vai pegando a proxima linha.
+
+print 'Pega uma linha: ', result
+
+
+query.execute('SELECT * FROM test_table')
+result = query.fetchall()	# Pega todas as linhas
+
+print '\nPega todas as linhas: ', result
