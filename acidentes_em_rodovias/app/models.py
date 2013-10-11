9949d618f88dda8b9ce0a22a7799b51391381b3c
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2013-10-11 19:39:18 -0300

    Adicionado mais entidades

--- acidentes_em_rodovias/app/models.py
+++ acidentes_em_rodovias/app/models.py
@@ -83,3 +83,38 @@ class ocorrenciaPessoa(Entidade):
                 self.opecondalegadas = ""
                 self.sem = 0
                 self.ano = 0
+
+class ocorrenciaacidente(Entidade):
+        def __init__(self):
+                self.oacocoid = 0
+                self.oacttacodigo = None
+                self.oactcacodigo = None
+                self.oacdano =  ""
+                self.oacdanoterc = ""
+                self.oacdanoamb = ""
+                self.oaclatitude = 0.0
+                self.oaclongitude = 0.0
+                self.oacdistab = 0.0
+                self.oacdistac = 0.0
+                self.oacdistbc = 0.0
+                self.oacmodelopista = 0
+                self.oacsentido1 = ""
+                self.oacsentido2 = ""
+                self.oacqtdfaixa1 = 0
+                self.oacqtdfaixa2 = 0
+                self.oacacostamento1 = ""
+                self.oacacostamento2 = ""
+                self.oaccanteiro = ""
+                self.oaclinhacentral =  ""
+                self.oacorientpista = ""
+                self.oacgirafundo = ""
+                self.oacversaocroqui = 0
+                self.oacsitio = 0
+                self.sem = 0
+                self.ano = 0
+
+class ocorrenciaveiculo(Entidade):
+        def __init__(self):
+                self.ocvid = 0
+                self.ocvocoid = None
+                self.ocvveiid = None
