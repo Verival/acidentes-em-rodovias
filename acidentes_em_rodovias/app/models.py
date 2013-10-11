87a030ac4f814de51d6ea6812d290fac87486e51
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2013-10-11 19:56:40 -0300

    Adicionando mais entidades e correção da classe Entidade

--- acidentes_em_rodovias/app/models.py
+++ acidentes_em_rodovias/app/models.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*- 
 
 class Entidade():
-        def __init__():
+        def __init__(self):
                 pass
 
-        def __str__():                
+        def __str__(self):                
 		attrs =vars(self)
 		s= ''.join("%s: %s\n" % item for item in attrs.items())
 		return s
@@ -118,3 +118,46 @@ class ocorrenciaveiculo(Entidade):
                 self.ocvid = 0
                 self.ocvocoid = None
                 self.ocvveiid = None
+
+class pessoa(Entidade):
+        def __init__(self):
+                self.pesid = 0
+                self.pesdatanascimento = ""
+                self.pesnaturalidade = ""
+                self.pesnacionalidade = ""
+                self.pessexo = ""
+                self.pesteccodigo = 0
+                self.pestgicodigo = 0
+                self.pesmunicipio = None
+                self.pestopcodigo = ""
+                self.pesmunicipioori = ""
+                self.pespaisori = ""
+                self.pesmunicipiodest = ""
+                self.pespaisdest = ""
+                self.pesveiid = None
+                self.pesestadofisico = None
+                self.pescinto = ""
+                self.pescapacete = ""
+                self.peshabilitado = ""
+                self.pesscorrido = ""
+                self.pesdormindo = ""
+                self.pesalcool = ""
+                self.peskmpercorre = ""
+                self.peshorapercorre = ""
+                self.pescategcnh = ""
+                self.pesufcnh = ""
+                self.pespaiscnh = ""
+                self.pesdatahabil = ""
+                self.pesdatavalidade = ""
+                self.pesidade = ""
+                self.pesaltura = ""
+                self.pespeso = ""
+                self.pescicatriz = ""
+                self.pestatuagem = ""
+                self.pessinal = ""
+                self.peslesao = ""
+                self.pestcccodigo = ""
+                self.pestctcodigo = ""
+                self.pestclcodigo = ""
+                self.pesoenid = ""
+                
