d68ea6b3fa19f3130a7b919a8bb0636e231fa842
Luiz <ziuloliveira@gmail.com>
2013-10-12 14:32:45 -0300

    Inclusão de try-except e metodo "troca_tabela", para reaproveitamente de objeto

--- acidentes_em_rodovias/app/dao.py
+++ acidentes_em_rodovias/app/dao.py
@@ -28,24 +28,54 @@ import pandas.io.sql as psql
 
 class DAO():
 	modelo=''
+	keys=[]
 	def __init__(self,modelo):
-		self.connection = util.get_connection()
+		try:
+			self.connection = util.get_connection()
 
-		self.modelo=modelo.lower()
-		dados = psql.frame_query("SHOW TABLES;",con=self.connection).to_dict('list')
-		dados = dados['Tables_in_acidentes_rodovias']
-		minimos = [i.lower() for i in dados]
-		if self.modelo in minimos:
-			self.modelo = dados[minimos.index(self.modelo)]
+			self.modelo=modelo.lower()
+			dados = psql.frame_query("SHOW TABLES;",con=self.connection).to_dict('list')
+			self.keys = dados['Tables_in_acidentes_rodovias']
+			minimos = [i.lower() for i in self.keys]
+			if self.modelo in minimos:
+				self.modelo = self.keys[minimos.index(self.modelo)]
+			else:
+				self.modelo=''
+				raise Exception("Tabela " + modelo + " não existente")
+		except Exception, ex:
+			print ex
+			print "Existentes:\n " + str(self.keys)
+			
+	def troca_tabela(self, modelo):
+		try:
+			minimos = [i.lower() for i in self.keys]
+			if modelo in minimos:
+				self.modelo = self.keys[minimos.index(modelo)]
+			else:
+				self.modelo=''
+				raise Exception("Tabela " + modelo + " não existente")
+		except Exception, ex:
+			print ex
+			print "Existentes:\n " + str(self.keys)
+			
 	
-	def consulta_todos(self):
-		dados = psql.frame_query("SELECT * FROM " +self.modelo+" LIMIT 10;",con=self.connection).to_dict()
-		classe = self.modelo
-		return util.transforma_para_objeto(dados, classe)
+	def consulta_todos(self,limit=0):
+		if(self.modelo != ''):
+			if(limit !=0):
+				limit = "LIMIT " + str(limit)
+			else:
+				limit = ''
+			query = "SELECT * FROM %s %s" %(self.modelo,limit)
+			dados = psql.frame_query(query,con=self.connection).to_dict()
+			return util.transforma_para_objeto(dados, self.modelo)
+		return []
 
 if __name__ == "__main__":
 	#exemplo de teste
 	dao = DAO('ocorrencia')
-	for i in dao.consulta_todos():
+	for i in dao.consulta_todos(3):
+		print i
+	dao.troca_tabela("essoa")
+	for i in dao.consulta_todos(3):
 		print i
 
