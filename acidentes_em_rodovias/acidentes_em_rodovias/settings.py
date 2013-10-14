d3f05b1fc5de9e675c3537b365479dd0c89ff359
Matheus Fonseca <1matheusfonseca@gmail.com>
2013-10-14 20:04:14 -0300

    Views melhoradas

--- acidentes_em_rodovias/acidentes_em_rodovias/settings.py
+++ acidentes_em_rodovias/acidentes_em_rodovias/settings.py
@@ -13,17 +13,19 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+TEST_RUNNER = 'app.tests.NoDbTestRunner'
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': '', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
