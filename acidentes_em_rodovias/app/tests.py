674b7b784b75bea68e11c536145101782158f246
Jose Pedro <1jpsneto@gmail.com>
2013-10-15 00:15:18 -0300

    Testes unitarios da classe DAO.

--- acidentes_em_rodovias/app/tests.py
+++ acidentes_em_rodovias/app/tests.py
@@ -8,10 +8,32 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import SimpleTestCase
-
-class SimpleTest(SimpleTestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+import dao
+        
+class TestDAO(SimpleTestCase):
+    
+    def test_existing_DAO_instance(self):
+        testDAO = dao.DAO('ocorrencia')
+        self.assertIsNotNone(testDAO)
+    
+    def test_existing_possiblesDAOS_instance(self):
+        testDAO = dao.DAO('ocorrencia')
+        for keyDAO in testDAO.keys:
+            possibleDAO = dao.DAO(keyDAO)
+            self.assertIsNotNone(possibleDAO)
+            
+    def test_consulta_todos(self):
+        testDAO = dao.DAO('ocorrencia')
+        for keyDAO in testDAO.keys:
+            possibleDAO = dao.DAO(keyDAO)
+            self.assertIsNotNone(possibleDAO.consulta_todos(2))
+            
+    def test_troca_tabela(self):
+        testDAO = dao.DAO('ocorrencia')
+        for keyDAO in testDAO.keys:
+            possibleDAO = dao.DAO(keyDAO)
+            actualPossibleDAO = possibleDAO 
+            changingPossibleDAO = possibleDAO.troca_tabela('causaacidente')
+            self.assertIsNotNone(changingPossibleDAO)
+            self.assertNotEquals(changingPossibleDAO, actualPossibleDAO)
+     
