e32b4eb5acd3c98853938cb7c54bf532f91d328f
Luiz <ziuloliveira@gmail.com>
2013-11-05 15:27:57 -0300

    maiores verificações de exceções nas chamadas de query

--- acidentes_em_rodovias/app/models/dao/municipio_dao.py
+++ acidentes_em_rodovias/app/models/dao/municipio_dao.py
@@ -8,9 +8,19 @@ class MunicipioDAO(GenericoDAO):
 		else:
 			limite = ''
 
-		query = """SELECT DISTINCT tmucodigo, tmudenominacao, tmuuf
-			FROM municipio tmu
-			INNER JOIN ocorrencia oco ON oco.ocomunicipio = tmu.tmucodigo
-	 		WHERE tmu.tmuuf = '%s' ORDER BY tmudenominacao %s; """ % (uf, limite)
-
+		try:
+			query = """SELECT DISTINCT tmucodigo, tmudenominacao, tmuuf
+				FROM municipio tmu
+				INNER JOIN ocorrencia oco ON oco.ocomunicipio = tmu.tmucodigo
+		 		WHERE tmu.tmuuf = '%s' ORDER BY tmudenominacao %s; """ % (uf, limite)
+		except OperationError as e:
+			sys.stderr.write("Falha na comunicação: " + str(e))
+			return None
+		except InternalError as e:
+			sys.stderr.write("Erro de sincronia: " + str(e))
+			return None
+		except NotSuportedError as e:
+			sys.stderr.write("Operação não suportada: " + str(e))
+			return None
+			
 		return self.transforma_dicionario_em_objetos(self.executa_query(query), "Municipio", "municipio")
--- acidentes_em_rodovias/app/models/dao/uf_dao.py
+++ acidentes_em_rodovias/app/models/dao/uf_dao.py
@@ -8,8 +8,17 @@ class UfDAO(GenericoDAO):
 			limite = 'LIMIT %s' % limite
 		else:
 			limite = ''
+		try:
+			query = """SELECT tufuf, tufdenominacao
+				FROM uf ORDER BY tufdenominacao %s;""" % limite
+		except OperationError as e:
+			sys.stderr.write("Falha na comunicação: " + str(e))
+			return None
+		except InternalError as e:
+			sys.stderr.write("Erro de sincronia: " + str(e))
+			return None
+		except NotSuportedError as e:
+			sys.stderr.write("Operação não suportada: " + str(e))
+			return None
 
-		query = """SELECT tufuf, tufdenominacao
-			FROM uf ORDER BY tufdenominacao %s;""" % limite
-		
 		return self.transforma_dicionario_em_objetos(self.executa_query(query), "Uf", "uf")
