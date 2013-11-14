4a638c40573e56d9e17251436c4318a052e3dfb8
Gepeto + Luiz <1jpsneto@gmail.com>
2013-11-14 14:11:08 -0300

    Refatoracao do modulo tests_DAO.

--- acidentes_em_rodovias/app/tests/__init__.py
+++ acidentes_em_rodovias/app/tests/__init__.py
@@ -1,2 +1,6 @@
 from tests_dao import *
-from tests_controller import *
+from tests_controller_consultabasica import *
+#from tests_controller_estatisticas import *
+from tests_dao_ocorrencia import *
+from tests_dao_municipio import *
+from tests_dao_UF import *
