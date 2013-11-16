cc14ad6ee73791f9e06cae5bb6149e332d917657
Gust <darksshades@hotmail.com>
2013-11-16 20:10:51 -0200

    Adiciona Django-Nose para identificar numero de testes passados e falhos para o sonar.

--- acidentes_em_rodovias/acidentes_em_rodovias/settings.py
+++ acidentes_em_rodovias/acidentes_em_rodovias/settings.py
@@ -121,13 +121,14 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'app',
 )
-
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
