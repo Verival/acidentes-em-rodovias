dbb8c402d8ac910e2eb89e390c6717f944ed738b
msfernandes <matheus.souza.fernandes@gmail.com>
2013-11-25 17:10:24 -0200

    Criação de DAO e model para busca das 10 rodovias com mais acidentes em todos os anos

---
+++ acidentes_em_rodovias/app/models/br_acidentes.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+class BRAcidentes(object):
+	def __init__(self):
+			self.quantidade_ocorrencias = ''
+			self.br = ''
+
+	def __str__(self):
+		return str(self.__dict__)
---
+++ acidentes_em_rodovias/app/models/dao/br_acidentes_dao.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from generico_dao import GenericoDAO
+import sys, os, inspect
+current_path = os.path.dirname(os.path.abspath('..'))
+sys.path.append(current_path)
+current_path = os.path.dirname(os.path.abspath('.'))
+sys.path.append(current_path)
+
+class BRAcidentesDAO(GenericoDAO):
+	def acidentes_br_geral(self):
+		query = """SELECT bre.br, bre.quantidade_ocorrencias
+				FROM estatisticas_br bre
+				ORDER BY bre.quantidade_ocorrencias DESC
+				LIMIT 10;"""
+
+		return self.transforma_dicionario_em_objetos(self.executa_query(query), 'BRAcidentes', 'br_acidentes')
