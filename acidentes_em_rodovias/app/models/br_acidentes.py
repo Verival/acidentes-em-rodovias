c603ca464968280053eb6874d3fe857ea3934a8b
msfernandes <matheus.souza.fernandes@gmail.com>
2013-11-26 09:51:49 -0200

    Criação de model e dao para consulta de acidentes por br em anos

--- acidentes_em_rodovias/app/models/br_acidentes.py
+++ acidentes_em_rodovias/app/models/br_acidentes.py
@@ -7,3 +7,12 @@ class BRAcidentes(object):
 
 	def __str__(self):
 		return str(self.__dict__)
+
+class BRAcidentesAno(object):
+	def __init__(self):
+			self.quantidade_ocorrencias = ''
+			self.br = ''
+			self.ano = ''
+
+	def __str__(self):
+		return str(self.__dict__)
--- acidentes_em_rodovias/app/models/dao/br_acidentes_dao.py
+++ acidentes_em_rodovias/app/models/dao/br_acidentes_dao.py
@@ -6,12 +6,26 @@ sys.path.append(current_path)
 current_path = os.path.dirname(os.path.abspath('.'))
 sys.path.append(current_path)
 
+from datetime import datetime
+
 class BRAcidentesDAO(GenericoDAO):
 	def acidentes_br_geral(self):
-		query = """SELECT bre.br, bre.quantidade_ocorrencias
+		query = """SELECT bre.br, SUM(bre.quantidade_ocorrencias) AS quantidade_ocorrencias
 				FROM estatisticas_br bre
 				GROUP BY bre.br
 				ORDER BY bre.quantidade_ocorrencias DESC
 				LIMIT 10;"""
-
 		return self.transforma_dicionario_em_objetos(self.executa_query(query), 'BRAcidentes', 'br_acidentes')
+
+	def acidentes_br_ano(self):
+		data = datetime.now()
+		acidentes_br = []
+		for ano in range(2007, data.year+1):
+			query = """SELECT bre.br, bre.quantidade_ocorrencias, bre.ano
+					FROM estatisticas_br bre
+					WHERE bre.ano = '%s'
+					GROUP BY bre.ano, bre.br
+					ORDER BY bre.quantidade_ocorrencias DESC;""" % ano
+			acidentes_ano = self.transforma_dicionario_em_objetos(self.executa_query(query), 'BRAcidentesAno', 'br_acidentes')
+			acidentes_br.append(acidentes_ano)
+		return acidentes_br
