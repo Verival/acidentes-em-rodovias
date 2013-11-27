6e299c30b3a911a1adb1fc2f0e005512cd08d8f9
Matheus Fonseca <1matheusfonseca@gmail.com>
2013-11-27 21:14:34 -0200

    Testes de interface para cinsulta por regiao

--- acidentes_em_rodovias/app/controller/consultabasica_controller.py
+++ acidentes_em_rodovias/app/controller/consultabasica_controller.py
@@ -4,6 +4,7 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
+import sys 
 import re
 import MySQLdb
 from django.utils.datastructures import MultiValueDictKeyError
