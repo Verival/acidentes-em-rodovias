f688deaeda1bec483df0f67cfb70e191fb319c2c
Luiz <ziuloliveira@gmail.com>
2013-12-02 17:47:47 -0200

    Testes agora inicializam serviro automaticamente e derruba ele ao final

--- acidentes_em_rodovias/test_coverage.sh
+++ acidentes_em_rodovias/test_coverage.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo -n "Removing *.pyc ------------------------------------------ "
-find . -type f -name "*.pyc" -exec rm -rf '{}' \;
+find . -type f -name "*.pyc" -exec rm -rf "{}" \;
 echo "Done"
-echo -n "Initing server ------------------------------------------"
-#gnome-terminal -e python manage.py runserver &
+echo -n "Initing server ------------------------------------------ "
+python manage.py runserver > /dev/null 2>&1 &
 echo "Done"
 coverage erase
 coverage run --source='app' manage.py test app
 coverage report -m
 coverage html -d ../../coverage
-
+echo -n "Killing everything -------------------------------------- "
+killall -9 coverage python test_coverage.sh -q
+echo "Killed"
