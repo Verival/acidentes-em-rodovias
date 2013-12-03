3f4c53be1fdf0af75585eac14406a3bc80faaf52
Luiz <ziuloliveira@gmail.com>
2013-12-03 12:09:33 -0200

    arrumada porta no sh

--- acidentes_em_rodovias/test_coverage.sh
+++ acidentes_em_rodovias/test_coverage.sh
@@ -3,7 +3,7 @@ echo -n "Removing *.pyc ------------------------------------------ "
 find . -type f -name "*.pyc" -exec rm -rf "{}" \;
 echo "Done"
 echo -n "Initing server ------------------------------------------ "
-python manage.py runserver 8080 &> /dev/null  &
+python manage.py runserver 8000 &> /dev/null  &
 echo "Done"
 coverage erase
 coverage run --source='app' manage.py test app
